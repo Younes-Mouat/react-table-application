e0bce37d8a21bf0fd80d95c4697bf3dbfc072655
Younes-Mouat <58032523+Younes-Mouat@users.noreply.github.com>
2021-01-23 03:17:14 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+//import axios from 'axios'
+import products_data from "./product_data.json"
+import ReactTable from "react-table"; 
+import 'react-table/react-table.css';
+//import { Form} from 'react-bootstrap';
+import TableFilter from 'react-table-filter';
+import 'react-table-filter/lib/styles.css';
 
-function App() {
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      //products: [],
+      //loading:true,
+      'updatedData': products_data
+    };
+    this._filterUpdated = this._filterUpdated.bind(this);
+  }
+
+  _filterUpdated(newData, filtersObject) {
+    this.setState({
+      'updatedData': newData,
+    });
+  }
+
+  render() {
+
+    const updatedData = this.state.updatedData;
+    const columns = [{  
+      Header: 'Features',  
+      accessor: 'features',
+     }
+     ,{  
+      Header: 'Specifications', 
+      columns: [
+        {
+          Header: "Name",
+          accessor: "specifications.0.name"
+        },
+        {
+          Header: "Category",
+          accessor: "specifications.0.category"
+        },
+        {
+          Header: "Value",
+          accessor: "specifications.0.value"
+        }
+      ] 
+      }
+    ,{  
+        Header: 'ID',  
+        accessor: '_id' ,
+    }
+    ,{  
+     Header: 'Name',  
+     accessor: 'name',
+     },
+     {  
+      Header: 'Description',  
+      accessor: 'description',
+      },
+      {  
+      Header: 'Category',  
+      accessor: 'category',
+      }
+      ,{  
+        Header: 'Subcategory',  
+        accessor: 'subcategory',
+    }
+    ,{  
+     Header: 'CreatedAt',  
+     accessor: 'createdAt',
+     },
+     {  
+      Header: 'UpdatedAt',  
+      accessor: 'updatedAt',
+      },
+      {  
+      Header: 'V',  
+      accessor: '__v',
+      }
+      ,{  
+        Header: 'ModelId',  
+        accessor: 'modelId' ,
+    }
+    ,{  
+     Header: 'PID',  
+     accessor: 'pid',
+     },
+     {  
+      Header: 'Datasheet',  
+      accessor: 'datasheet',
+      },
+      {  
+      Header: 'Link',  
+      accessor: 'link',
+      }
+      ,{  
+        Header: 'Thumbnail',  
+        accessor: 'thumbnail' ,
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactTable  
+    defaultPageSize={10}
+    data={updatedData} 
+    /*filtered={[{ 
+      id: 'subType',
+      value: this.state.subtype,
+    }]}*/ 
+    columns={columns}  
+ />
+ <TableFilter
+  rows={updatedData}
+  onFilterUpdate={this._filterUpdated}>
+  <th key="category" filterkey="category" className="cell" casesensitive={'true'} showsearch={'true'}>
+    Category
+  </th>
+  <th filterkey="subcategory">
+    Subcategory
+  </th>
+</TableFilter>
     </div>
-  );
+    
+    
+  )
+  }
 }
-
-export default App;
